30bfa4f4f40c0803de100030d71378eaca66ceb2
MatyusGI <48155477+MatyusGI@users.noreply.github.com>
2019-11-21 19:52:56 +0700

    Update XGBoost.py

--- XGBoost.py
+++ XGBoost.py
@@ -7,3 +7,12 @@ y_tr = pd.read_csv('tox21_labels_train.csv.gz', index_col=0, compression="gzip")
 y_te = pd.read_csv('tox21_labels_test.csv.gz', index_col=0, compression="gzip")
 x_tr = pd.read_csv('tox21_dense_train.csv.gz', index_col=0, compression="gzip")
 x_te = pd.read_csv('tox21_dense_test.csv.gz', index_col=0, compression="gzip")
+
+rows_tr = np.isfinite(y_tr['SR.p53']).values
+rows_te = np.isfinite(y_te['SR.p53']).values
+y_train = y_tr['SR.p53'][rows_tr]
+x_train = x_tr[rows_tr]
+y_test = y_te['SR.p53'][rows_te]
+x_test = x_te[rows_te]
+
+y_train
